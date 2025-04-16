fd98f8b027ce375a52d262ff05228d7c68eacefe
Khang Huynh Ba <kanghuynhba@gmail.com>
2025-04-16 20:35:17 +0700

    continue about numpy tutor

--- test.py
+++
@@ -1,8 +0,0 @@
-import tensorflow as tf
-
-# Download and import the MIT Introduction to Deep Learning package
-!pip install mitdeeplearning --quiet
-import mitdeeplearning as mdl
-
-import numpy as np
-import matplotlib.pyplot as plt
